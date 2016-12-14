abbd09c1aa475825a3bb339d2e4ca122769a22db
Dor Tzur <dortzur@gmail.com>
2016-12-14 23:55:46 +0200

    MS game deck

--- packages/engine/src/creators/game-creator.js
+++ packages/engine/src/creators/game-creator.js
@@ -1,5 +1,6 @@
 import {RANKS, SUITS} from "../constants";
 import {createCard} from "./card-creator";
+import srand from "../utils/srand";
 export const createDeck = () => {
   const deck = [];
   for (let i = 0; i < 13; i++) {
@@ -11,6 +12,17 @@ export const createDeck = () => {
   return deck;
 };
 
+export const createGameDeck = (gameNumber) => {
+  let deck = createDeck();
+  const rand = srand(gameNumber);
+  for (let i = deck.length - 1; i > 0; i--) {
+    const r = rand() % (i + 1);
+    const card = deck[r];
+    deck[r] = deck[i];
+    deck[i] = card;
+  }
+  return deck.reverse();
+};
 export const createGame = (gameNumber) => {
 
 };
--- packages/engine/test/game.spec.js
+++ packages/engine/test/game.spec.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {createDeck} from '../src/creators/game-creator';
+import {createDeck, createGameDeck} from '../src/creators/game-creator';
 import {kingOfClubs} from "./fixtures/cards";
 
 describe('game-creator', () => {
@@ -7,9 +7,16 @@ describe('game-creator', () => {
   it('creates a deck of cards', () => {
     const deck = createDeck();
     expect(deck[51]).to.deep.eq(kingOfClubs);
-
-
-    console.log(deck);
   });
 
+  it('creates a game deck according to MS game numbers', () => {
+
+    let deck = createGameDeck(1);
+    expect(deck[0].notation).to.eq("JD");
+    expect(deck[51].notation).to.eq("6H");
+
+    deck = createGameDeck(617);
+    expect(deck[0].notation).to.eq("7D");
+    expect(deck[51].notation).to.eq("4H");
+  })
 });
