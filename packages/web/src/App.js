30555412f28aa0c80c1ae63594e8dba39d18a185
Dor Tzur <dortzur@gmail.com>
2016-12-20 01:09:33 +0200

    add card lib + initial css

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Cell from "./cell.js"
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div className="free-cells">
+                    <Cell/>
+                    <Cell/>
+                    <Cell/>
+                    <Cell/>
+                </div>
+                <div className="home-cells">
+                    <Cell/>
+                    <Cell/>
+                    <Cell/>
+                    <Cell/>
+                </div>
+
+                <div className="column-cells">
+                    <Cell/>
+                    <Cell/>
+                    <Cell/>
+                    <Cell/>
+                    <Cell/>
+                    <Cell/>
+                    <Cell/>
+                    <Cell/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ packages/web/src/cell.js
@@ -0,0 +1,9 @@
+import React, {Component, PropTypes} from "react";
+export default class Cell extends Component{
+
+    render() {
+        return (<div className="cell">
+
+        </div>)
+    }
+}
