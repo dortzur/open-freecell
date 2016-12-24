ef495563e20df2f635660f4cdf48d04a7b7e508d
Dor Tzur <dortzur@gmail.com>
2016-12-24 21:32:27 +0200

    building like a boss

--- packages/engine/index.js
+++
@@ -1,3 +0,0 @@
-export {moveToColumnCell, moveToFreeCell, moveToHomeCell} from "./src/state/actions"
-export reducer from "./src/state/reducer"
-export {getGameObj, getHomeCells, getFreeCells, getColumnCells} from "./src/state/selectors";
---
+++ packages/engine/src/index.js
@@ -0,0 +1,3 @@
+export {moveToColumnCell, moveToFreeCell, moveToHomeCell} from "./state/actions"
+export reducer from "./state/reducer"
+export {getGameObj, getHomeCells, getFreeCells, getColumnCells} from "./state/selectors";
