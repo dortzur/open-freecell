ea823e7c50360dcc055a5d4b6ca360cfa2f72bab
Dor Tzur <dortzur@gmail.com>
2016-12-25 00:24:26 +0200

    export print

--- packages/engine/src/index.js
+++ packages/engine/src/index.js
@@ -1,3 +1,4 @@
 export {moveToColumnCell, moveToFreeCell, moveToHomeCell, newGame} from "./state/actions"
 export reducer from "./state/reducer"
 export {getGameObj, getHomeCells, getFreeCells, getColumnCells} from "./state/selectors";
+export {printGame, gameToString} from "./services/print-service";
