a8adcc949c8a2c79b50ec54738572bbf584aa606
Dor Tzur <dortzur@gmail.com>
2016-12-25 00:55:27 +0200

    working game

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -21,11 +21,17 @@ cli.command("home <card> <cellNumber>", "move a specified card to home cells 1-4
   cli.ui.redraw(gameToString(store.getState().game));
   cli.ui.redraw.done();
 });
-cli.command("free <card> <cellNumber>", "move a specified card to free cells 1-4. Example: free AS 1")
+cli.command("free <card> <cellNumber>", "move a specified card to free cells 1-4. Example: free QH 1")
   .alias('f').action(async(args) => {
   store.dispatch(moveToFreeCell(args.card.toUpperCase(), args.cellNumber - 1));
   cli.ui.redraw(gameToString(store.getState().game));
   cli.ui.redraw.done();
 });
 
+cli.command("col <card> <cellNumber>", "move a specified card/card stack to column cells 1-8. Example: col JD 7")
+  .alias('c').action(async(args) => {
+  store.dispatch(moveToColumnCell(args.card.toUpperCase(), args.cellNumber - 1));
+  cli.ui.redraw(gameToString(store.getState().game));
+  cli.ui.redraw.done();
+});
 cli.show();
