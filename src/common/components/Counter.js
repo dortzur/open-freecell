d302e70cfa3cf30631e571e7ac79941a37a19b5d
Dor Tzur <dortzur@gmail.com>
2018-04-06 16:00:21 +0300

    fix issue ˆ

--- src/common/components/Counter.js
+++ src/common/components/Counter.js
@@ -1,14 +1,14 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
-import * as CounterActions from "../actions";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as CounterActions from '../actions';
 
 @connect(
-  state => ({
-    counter: state.counter
+  (state) => ({
+    counter: state.counter,
   }),
-  bindActionCreators(CounterActions, dispatch)
+  (dispatch) => bindActionCreators(CounterActions, dispatch)
 )
 export default class Counter extends React.PureComponent {
   static propTypes = {
@@ -16,7 +16,7 @@ export default class Counter extends React.PureComponent {
     incrementIfOdd: PropTypes.func.isRequired,
     incrementAsync: PropTypes.func.isRequired,
     decrement: PropTypes.func.isRequired,
-    counter: PropTypes.number.isRequired
+    counter: PropTypes.number.isRequired,
   };
 
   render() {
@@ -25,13 +25,13 @@ export default class Counter extends React.PureComponent {
       incrementIfOdd,
       incrementAsync,
       decrement,
-      counter
+      counter,
     } = this.props;
     return (
       <p>
-        Clicked: {counter} times <button onClick={increment}>+</button>{" "}
-        <button onClick={decrement}>-</button>{" "}
-        <button onClick={incrementIfOdd}>Increment if odd</button>{" "}
+        Clicked: {counter} times <button onClick={increment}>+</button>{' '}
+        <button onClick={decrement}>-</button>{' '}
+        <button onClick={incrementIfOdd}>Increment if odd</button>{' '}
         <button onClick={() => incrementAsync()}>Increment async</button>
       </p>
     );
--- src/common/containers/App.js
+++ src/common/containers/App.js
@@ -1,4 +1,3 @@
-
-import Counter from "../components/Counter";
+import Counter from '../components/Counter';
 
 export default Counter;
