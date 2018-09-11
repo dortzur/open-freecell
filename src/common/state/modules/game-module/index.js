5731734315287dc277158f0f718a52be98eac970
Dor Tzur <dortzur@gmail.com>
2018-09-11 21:35:14 +0300

    denormalize

--- src/common/state/modules/game-module/index.js
+++ src/common/state/modules/game-module/index.js
@@ -16,9 +16,9 @@ export default (state = initialState, action) => {
     case types.START_GAME: {
       const { gameDeck } = action.payload;
 
-      const data = normalize({ deck: gameDeck }, schema);
-      const deck = data.result.deck;
-
+      // const data = normalize({ deck: gameDeck }, schema);
+      // const deck = data.result.deck;
+      const deck = gameDeck;
       state.tableau = produce(state.tableau, (tableau) =>
         deck.reduce((tableau, card, index) => {
           tableau[index % 8].push(card);
